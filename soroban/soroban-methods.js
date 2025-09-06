be4b043a9c9ef0ae0d0e02076bf38173ba78e3ea
TinkerPal <tinkerpi2023@gmail.com>
2025-09-06 04:04:17 +0300

    updated load account logic

--- soroban/soroban-methods.js
+++ soroban/soroban-methods.js
@@ -182,12 +182,9 @@ async function invokeContract(network, contractId, operation, args) {
 
     console.log("fine here 1");
     // source & builder (fresh Account object)
-    const info = await server.getAccount(sourceId);
+    const source = await server.getAccount(sourceId);
 
-    console.log("the info", info);
-    const acc = new Account(sourceId, info.sequence);
-
-    let tx = new TransactionBuilder(acc, {
+    let tx = new TransactionBuilder(source, {
       fee: BASE_FEE,
       networkPassphrase: Networks[network],
     })
