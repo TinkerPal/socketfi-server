0da893b32d7b6f7c3f5b4bbe90430b540d1ca623
TinkerPal <tinkerpi2023@gmail.com>
2026-03-20 13:39:43 +0300

    updated email config

--- signer-nodes/signer-nodes.js
+++ signer-nodes/signer-nodes.js
@@ -1,5 +1,5 @@
 const nodes = [
-  { name: "node 1", url: "http://164.68.126.247:3000" },
+  { name: "node 1", url: "http://164.68.126.247:3005" },
   { name: "node 2", url: "http://38.242.195.168:3000" },
   { name: "node 3", url: "http://144.91.98.211:3000" },
 ];
