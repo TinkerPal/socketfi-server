4d4ba21c6b7415da483d7d3f52df18571bb36dbe
TinkerPal <tinkerpi2023@gmail.com>
2026-03-20 11:31:44 +0300

    updated validator link

--- signer-nodes/signer-nodes.js
+++ signer-nodes/signer-nodes.js
@@ -1,7 +1,7 @@
 const nodes = [
-  { name: "node 1", url: "https://socketfi-signer-node-1.socket.fi" },
-  { name: "node 2", url: "https://socketfi-signer-node-2.socket.fi" },
-  { name: "node 3", url: "https://socketfi-signer-node-3.socket.fi" },
+  { name: "node 1", url: "http://164.68.126.247:3000" },
+  { name: "node 2", url: "http://38.242.195.168:3000" },
+  { name: "node 3", url: "http://144.91.98.211:3000" },
 ];
 
 module.exports = nodes;
